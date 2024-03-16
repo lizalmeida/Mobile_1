8b23a1663e1e04ee982417acd2403a5b75fef4a3
lizalmeida <lizalmeidac73@gmail.com>
2024-03-16 14:09:35 -0500

    Update MainActivity.kt
    Modifica archivo MainActivity

--- app/src/main/java/com/example/mobile_1/MainActivity.kt
+++ app/src/main/java/com/example/mobile_1/MainActivity.kt
@@ -3,14 +3,18 @@ package com.example.mobile_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.mobile_1.ui.theme.Mobile_1Theme
 
@@ -32,12 +36,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        color = Color.Green,
-        fontSize = 40.sp
-    )
+fun Greeting(name: String) {
+    Box (modifier = Modifier
+        .size(400.dp)){
+        Text(
+            text = "Text1",
+            color = Color.Blue,
+            fontSize = 20.sp,
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+            )
+        Text(
+             text = "Text2",
+             color = Color.Blue,
+             fontSize = 20.sp,
+             modifier = Modifier
+                .align(Alignment.BottomStart)
+            )
+    }
 }
 
 @Preview(showBackground = true)
