9be1b0687839838af839acdff207f781c22fda47
lizalmeida <lizalmeidac73@gmail.com>
2024-03-22 21:03:49 -0500

    Timer
    Timer

--- app/src/main/java/com/example/mobile_1/PersonList.kt
+++ app/src/main/java/com/example/mobile_1/PersonList.kt
@@ -1,5 +1,6 @@
 package com.example.mobile_1
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,13 +11,16 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -25,17 +29,31 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.platform.LocalContext
-import java.util.UUID
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.delay
 
 @Composable
 fun PersonList() {
-    var person by remember {
-        mutableStateOf(Person(UUID.randomUUID(),"", 0))
+    var timeLeft by remember {
+        mutableIntStateOf(10)
+    }
+    LaunchedEffect(key1 = timeLeft) {
+        while (timeLeft > 0){
+            delay(1000L)
+            timeLeft --
+        }
+    }
+    var name by remember {
+        mutableStateOf("")
+    }
+    var age by remember {
+        mutableStateOf("")
     }
     var persons by remember {
         mutableStateOf(listOf<Person>())
     }
 
+    val context = LocalContext.current
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -45,13 +63,41 @@ fun PersonList() {
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
                 .fillMaxWidth()
+        ){
+            Text(
+                text = "Time left: ${timeLeft.toString()}",
+                modifier = Modifier
+                    .padding(end = 16.dp),
+                fontSize = 20.sp
+            )
+            Spacer(modifier = Modifier
+                .weight(1f))
+
+            Button(modifier = Modifier
+                .padding(end = 16.dp),
+                onClick = {
+                    timeLeft = 10
+                }
+            )
+            {
+                Icon(
+                    imageVector = Icons.Default.Refresh,
+                    contentDescription = ""
+                )
+            }
+        }
+
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
                 .padding(16.dp)
         ){
             TextField(
-                value = person.name,
+                value = name,
                 placeholder = {Text("Enter name")},
                 onValueChange = { text ->
-                    person.name = text
+                    name= text
                 }
             )
             Spacer(modifier = Modifier
@@ -65,10 +111,10 @@ fun PersonList() {
                 .padding(16.dp)
         ){
             TextField(
-                value = person.age,
+                value = age,
                 placeholder = {Text("Enter age")},
                 onValueChange = { text ->
-                    person.age = text
+                    age= text
                 }
             )
             Spacer(modifier = Modifier
@@ -78,8 +124,19 @@ fun PersonList() {
         Button(modifier = Modifier
             .padding(end = 16.dp),
             onClick = {
-                persons = persons + person
-                person = Person(UUID.randomUUID(), "", 0)
+                if (name.isNotBlank() && age.isNotBlank()){
+                    persons = persons + Person(name = name, age = age.toInt())
+                } else {
+                    Toast
+                        .makeText(
+                            context,
+                            "Enter a valid name",
+                            Toast.LENGTH_SHORT
+                        )
+                        .show()
+                }
+                name = ""
+                age = ""
             }
         )
         {
@@ -88,12 +145,22 @@ fun PersonList() {
                 contentDescription = "Icono de añadir"
             )
         }
+
         LazyColumn{
-            items(persons){currentPersona ->
-                Text(text = "${currentPersona.name}, ${currentPersona.age}",
-                modifier = Modifier
-                    .padding(16.dp)
-                )
+            items(persons){currentPerson ->
+                Row {
+                    Text(
+                        text = currentPerson.name,
+                        modifier = Modifier
+                            .padding(16.dp)
+                    )
+                    Spacer(modifier = Modifier.weight(1f))
+                    Text(
+                        text = currentPerson.age.toString(),
+                        modifier = Modifier
+                            .padding(16.dp)
+                    )
+                }
                 Divider()
             }
         }
@@ -101,6 +168,6 @@ fun PersonList() {
 }
 @Preview(showBackground = true)
 @Composable
-fun PersonListPreview() {
+fun ListPersonPreview() {
     PersonList()
 }
